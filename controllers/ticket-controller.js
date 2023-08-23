3f91f8d55e4217d8dec610c51cedaf3d59769ca4
JC Soriano <juancarlosoriano18@gmail.com>
2023-08-24 01:35:56 +0300

    Added comment endpoint fix

--- controllers/ticket-controller.js
+++ controllers/ticket-controller.js
@@ -190,6 +190,7 @@ const createComment = async (req, res) => {
 
   try {
     let ticket = await Ticket.findOne({ _id: ticket_id }).exec();
+    console.log(ticket);
     await ticket.comments.push(comment);
     await ticket.save();
 
